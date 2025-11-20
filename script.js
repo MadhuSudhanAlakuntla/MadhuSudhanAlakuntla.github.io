6fdb6709abf953782f57fc33fcf70c38e4bf8ecf
Madhu Sudhan <135996472+MadhuSudhanAlakuntla@users.noreply.github.com>
2025-11-20 15:15:59 +0530

    Refactor scroll and reveal functions
    Refactored scroll and reveal functions for better readability and efficiency.

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-
 // scroll bar script
-window.onscroll = function() {
-    myFunction()
-};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-
-
-
-// smooth up script
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+window.onscroll = function ()
+ { myFunction() };
+  function myFunction() { 
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight; 
+     var scrolled = (winScroll / height) * 100; document.getElementById("myBar").style.width = scrolled + "%"; 
     }
-  }
-  
-  window.addEventListener("scroll", reveal);
 
-  
+    // smooth up script
+    function reveal() { var reveals =
+        document.querySelectorAll(".reveal");
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop =
+            reveals[i].getBoundingClientRect().top;
+             var elementVisible = 150;
+             if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+                } else {
+                    reveals[i].classList.remove("active");
+                    }
+                    }
+                    }
+                    window.addEventListener("scroll", reveal);
+                    // about box script
+                    let tablinks = document.getElementsByClassName("tab-links");
+                    let tabcontents = document.getElementsByClassName("tab-contents");
 
-// about box script
-let tablinks = document.getElementsByClassName("tab-links");
-let tabcontents = document.getElementsByClassName("tab-contents");
+                    function opentab(tabname){
+                        for(let i of tablinks){
+                            i.classList.remove("active-link");
+                        }
+                        for(let i of tabcontents){
+                            i.classList.remove("active-tab");
+                        }
+                        event.currentTarget.classList.add("active-link");
+                        document.getElementById(tabname).classList.add("active-tab");
+                    }// Close mobile menu when any menu item is clicked
+const menuToggle = document.getElementById("menu-toggle");
+const menuLinks = document.querySelectorAll(".menu a");
 
-function opentab(tabname){
-    for(let i of tablinks){
-        i.classList.remove("active-link");
-    }
-    for(let i of tabcontents){
-        i.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        if (menuToggle.checked) {
+            menuToggle.checked = false;  // CLOSE MENU
+        }
+    });
+});
+
+
+
